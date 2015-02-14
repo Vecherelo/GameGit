2cbc200c353d1d9aee149803b685731bdc7365f3
Vecherelo <mogorin@gmail.com>
2015-02-14 14:09:33 +0300

    add to project

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pl = new System.Windows.Forms.Panel();
+            this.picUmen1 = new System.Windows.Forms.PictureBox();
+            this.picUmen2 = new System.Windows.Forms.PictureBox();
+            this.picUmen3 = new System.Windows.Forms.PictureBox();
+            this.picUmen4 = new System.Windows.Forms.PictureBox();
+            this.picUmen5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.pl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picUmen1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUmen2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUmen3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUmen4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUmen5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(19, 146);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(171, 139);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Жизни";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "100";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(74, 288);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(190, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(399, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 155);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Удар";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.progressBar1.Cursor = System.Windows.Forms.Cursors.No;
+            this.progressBar1.ForeColor = System.Drawing.Color.DarkRed;
+            this.progressBar1.Location = new System.Drawing.Point(19, 117);
+            this.progressBar1.MarqueeAnimationSpeed = 0;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(171, 23);
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Value = 100;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.progressBar2.Location = new System.Drawing.Point(40, 12);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(209, 23);
+            this.progressBar2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(131, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(255, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "100";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(19, 146);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(343, 147);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(636, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 36);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Новая Игра";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(636, 54);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 36);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "ЗАГРУЗИТЬ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(636, 96);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 32);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "СОХРАНИТЬ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pl
+            // 
+            this.pl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pl.Controls.Add(this.picUmen5);
+            this.pl.Controls.Add(this.picUmen4);
+            this.pl.Controls.Add(this.picUmen3);
+            this.pl.Controls.Add(this.picUmen2);
+            this.pl.Controls.Add(this.picUmen1);
+            this.pl.Location = new System.Drawing.Point(340, 12);
+            this.pl.Name = "pl";
+            this.pl.Size = new System.Drawing.Size(200, 36);
+            this.pl.TabIndex = 16;
+            // 
+            // picUmen1
+            // 
+            this.picUmen1.Image = ((System.Drawing.Image)(resources.GetObject("picUmen1.Image")));
+            this.picUmen1.Location = new System.Drawing.Point(4, 5);
+            this.picUmen1.Name = "picUmen1";
+            this.picUmen1.Size = new System.Drawing.Size(34, 24);
+            this.picUmen1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picUmen1.TabIndex = 0;
+            this.picUmen1.TabStop = false;
+            // 
+            // picUmen2
+            // 
+            this.picUmen2.Image = ((System.Drawing.Image)(resources.GetObject("picUmen2.Image")));
+            this.picUmen2.Location = new System.Drawing.Point(44, 5);
+            this.picUmen2.Name = "picUmen2";
+            this.picUmen2.Size = new System.Drawing.Size(30, 24);
+            this.picUmen2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picUmen2.TabIndex = 0;
+            this.picUmen2.TabStop = false;
+            // 
+            // picUmen3
+            // 
+            this.picUmen3.Image = ((System.Drawing.Image)(resources.GetObject("picUmen3.Image")));
+            this.picUmen3.Location = new System.Drawing.Point(80, 3);
+            this.picUmen3.Name = "picUmen3";
+            this.picUmen3.Size = new System.Drawing.Size(30, 24);
+            this.picUmen3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picUmen3.TabIndex = 0;
+            this.picUmen3.TabStop = false;
+            this.picUmen3.Click += new System.EventHandler(this.picUmen3_Click);
+            // 
+            // picUmen4
+            // 
+            this.picUmen4.Image = ((System.Drawing.Image)(resources.GetObject("picUmen4.Image")));
+            this.picUmen4.Location = new System.Drawing.Point(116, 3);
+            this.picUmen4.Name = "picUmen4";
+            this.picUmen4.Size = new System.Drawing.Size(30, 24);
+            this.picUmen4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picUmen4.TabIndex = 0;
+            this.picUmen4.TabStop = false;
+            // 
+            // picUmen5
+            // 
+            this.picUmen5.Image = ((System.Drawing.Image)(resources.GetObject("picUmen5.Image")));
+            this.picUmen5.Location = new System.Drawing.Point(152, 3);
+            this.picUmen5.Name = "picUmen5";
+            this.picUmen5.Size = new System.Drawing.Size(30, 24);
+            this.picUmen5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picUmen5.TabIndex = 0;
+            this.picUmen5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 335);
+            this.Controls.Add(this.pl);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.pl.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picUmen1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUmen2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUmen3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUmen4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUmen5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel pl;
+        private System.Windows.Forms.PictureBox picUmen5;
+        private System.Windows.Forms.PictureBox picUmen4;
+        private System.Windows.Forms.PictureBox picUmen3;
+        private System.Windows.Forms.PictureBox picUmen2;
+        private System.Windows.Forms.PictureBox picUmen1;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Game
+{
+    
+    public partial class Form1 : Form
+    {
+        int Exp=0;
+        int countBoss = 2;
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int B=0, U=0;
+            B = Int32.Parse(label2.Text);
+            U = rnd.Next(0, 30);
+            label3.Text = U.ToString();
+            label2.Text = (B-U).ToString();
+            if ((B - U) < 0) { 
+                label2.Text = "0";
+                
+                label4.Text = "ПОБЕДИЛИ !!!!!!!!!!";
+                pictureBox1.Visible = false;
+           }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+
+            int Lvl = Int32.Parse(label5.Text);
+            int U = (10 + Lvl * 2) + rnd.Next(-5, 5);
+
+            label3.Font = new Font(label3.Font.Name, 15,
+                label3.Font.Style);
+            label3.ForeColor = Color.Lime;
+            if (rnd.Next(0, 100) <= 15)
+            {
+                U *= 3;
+                label3.ForeColor = Color.Red;
+                label3.Font = new Font(label3.Font.Name, 35,
+                label3.Font.Style);
+            }
+
+            label3.Text = U.ToString();
+
+            if ((progressBar1.Value - U) >= 0)
+            {
+
+                progressBar1.Value -= U;
+                label2.Text = progressBar1.Value.ToString();
+            }
+            else
+            {
+                progressBar1.Value = 0;
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = false;
+                label3.Text = "0";
+
+                Application.DoEvents();
+                //  System.Threading.Thread.Sleep(100);
+                if (countBoss == 1) { Exp += 30; }
+                else { Exp += 6; }
+
+                countBoss++;
+                if (countBoss == 5)
+                {
+                    countBoss = 1;
+                    pictureBox2.Visible = true;
+                    progressBar1.Maximum = 300;
+                }
+                else
+                {
+                    pictureBox1.Visible = true;
+                    progressBar1.Maximum = 100;
+
+                }
+                progressBar1.Value = progressBar1.Maximum;
+
+
+                if (Exp > 100) { Exp -= 100; label5.Text = (Int32.Parse(label5.Text) + 1).ToString(); }
+                progressBar2.Value = Exp;
+                label6.Text = Exp.ToString();
+                label2.Text = progressBar1.Value.ToString();
+
+
+
+
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*foreach (Control control in Controls)
+            {
+                control.Visible = false;
+            }
+            button2.Visible = true;*/
+            label5.Text = "1";  //LVL
+            label6.Text = "0";  // EXP
+            label2.Text = "100"; //HP Mob
+            label3.Text = "0"; // Damage
+            
+            progressBar2.Value = 0;
+            progressBar1.Value = 100;
+            progressBar1.Maximum = 100;
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = false;
+            countBoss = 2;
+            Exp = 0;
+            
+           
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+            File.WriteAllLines(@"D:\text.txt", new string[] { Exp.ToString(), label5.Text });
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Exp = Convert.ToInt32(File.ReadAllLines(@"D:\text.txt")[0]);
+            label5.Text = File.ReadAllLines(@"D:\text.txt")[1];
+            progressBar2.Value = Exp;
+            label6.Text = Exp.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            File.WriteAllLines(@"D:\text.txt", new string[] { Exp.ToString(), label5.Text });
+        }
+
+        private void picUmen3_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+
+            int Lvl = Int32.Parse(label5.Text);
+            int U = (30 + Lvl * 2) + rnd.Next(-5, 5);
+
+            label3.Font = new Font(label3.Font.Name, 15,
+                label3.Font.Style);
+            label3.ForeColor = Color.Lime;
+            if (rnd.Next(0, 100) <= 15)
+            {
+                U *= 3;
+                label3.ForeColor = Color.Red;
+                label3.Font = new Font(label3.Font.Name, 35,
+                label3.Font.Style);
+            }
+
+            label3.Text = U.ToString();
+
+            if ((progressBar1.Value - U) >= 0)
+            {
+
+                progressBar1.Value -= U;
+                label2.Text = progressBar1.Value.ToString();
+            }
+            else
+            {
+                progressBar1.Value = 0;
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = false;
+                label3.Text = "0";
+
+                Application.DoEvents();
+                //  System.Threading.Thread.Sleep(100);
+                if (countBoss == 1) { Exp += 30; }
+                else { Exp += 6; }
+
+                countBoss++;
+                if (countBoss == 5)
+                {
+                    countBoss = 1;
+                    pictureBox2.Visible = true;
+                    progressBar1.Maximum = 300;
+                }
+                else
+                {
+                    pictureBox1.Visible = true;
+                    progressBar1.Maximum = 100;
+
+                }
+                progressBar1.Value = progressBar1.Maximum;
+
+
+                if (Exp > 100) { Exp -= 100; label5.Text = (Int32.Parse(label5.Text) + 1).ToString(); }
+                progressBar2.Value = Exp;
+                label6.Text = Exp.ToString();
+                label2.Text = progressBar1.Value.ToString();
+
+
+
+
+            }
+        }
+            
+
+        }
+
+      
+  }
+
