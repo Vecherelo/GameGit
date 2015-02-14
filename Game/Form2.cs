3e39d8299a79516f9740a04a144f514847df8e68
Vecherelo <mogorin@gmail.com>
2015-02-14 14:13:41 +0300

    yes

--- Game/Form2.cs
+++ Game/Form2.cs
@@ -20,6 +20,8 @@ namespace Game
         private void button1_Click(object sender, EventArgs e)
         {
             textBox1.Text = "sdfsdf" + "sdfsdf";
+            int t = 5;
+            t += 5;
             Close();
 
         }
